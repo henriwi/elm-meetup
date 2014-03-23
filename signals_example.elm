47cf7f66b420f2465198efd024cef9026d4ba8d4
Joel Chelliah <joel.chelliah@bekk.no>
2014-03-23 14:42:13 +0100

    tidied up signals example

--- signals_example.elm
+++ signals_example.elm
@@ -10,20 +10,21 @@ input = sampleOn (fps 20) (Input <~ Mouse.position
                                   ~ Keyboard.shift)
                                   
 
-display input = let drawClicks = toText (show input.mouseClicks ++ " clicks") |> Text.height 30
-                                                                              |> text
-                                                                              |> toForm
-                                                                              |> move (10,0)
-                    drawMouseP = toText ("position: " ++ show input.mousePos) |> Text.height 30
-                                                                              |> text
-                                                                              |> toForm
-                                                                              |> move (10,50)
-                    drawShift = toText (if input.shift then "SHIFT!" else "") |> Text.height 50
-                                                                              |> Text.color red
-                                                                              |> text
-                                                                              |> toForm
-                                                                              |> move (10,100)
+display input = let drawClicks = show input.mouseClicks ++ " clicks" |> formText (Text.color blue)
+                                                                     |> move (10,0)
+                                                                     
+                    drawMouseP = "position: " ++ show input.mousePos |> formText (Text.color brown)
+                                                                     |> move (10,50)
+                                                                     
+                    shiftText = if input.shift then "SHIFT!" else ""
+                    drawShift = shiftText |> formText ((Text.color red) . bold)
+                                          |> move (10,100)
                 in collage 400 400 [drawClicks, drawMouseP, drawShift]
+
+
+formText f = toForm . text . f . (Text.height 30) . toText
+
+
 
 --main = asText <~ input
 main = display <~ input
