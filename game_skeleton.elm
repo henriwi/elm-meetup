61a51dbf0f106f453f7b1c02c040692e330f92c6
Joel Chelliah <joel.chelliah@bekk.no>
2014-03-29 17:36:58 +0100

    Worked on game example and skeleton

--- game_skeleton.elm
+++ game_skeleton.elm
@@ -1,41 +1,38 @@
 import Window
 
-{-- Part 1: Model the user input ----------------------------------------------
-------------------------------------------------------------------------------}
+{-- Part 1: Model the user input --------}
 
-type UserInput = {}
-
-userInput : Signal UserInput
-userInput = constant {}
-
-type Input = { timeDelta:Float, userInput:UserInput }
+type Input = { time:Float }
 
 
-{-- Part 2: Model the game ----------------------------------------------------
-------------------------------------------------------------------------------}
 
-type GameState = {}
 
-defaultGame : GameState
+{-- Part 2: Model the game ---------------}
+
+type Game = {}
+
+defaultGame : Game
 defaultGame = {}
 
 
-{-- Part 3: Update the game ---------------------------------------------------
-------------------------------------------------------------------------------}
-
-stepGame : Input -> GameState -> GameState
-stepGame {timeDelta,userInput} gameState = gameState
 
 
-{-- Part 4: Display the game --------------------------------------------------
-------------------------------------------------------------------------------}
+{-- Part 3: Update the game ---------------}
 
-display : (Int,Int) -> GameState -> Element
-display (w,h) gameState = asText gameState
+stepGame : Input -> Game -> Game
+stepGame {time} g = g
+
+
+
+
+{-- Part 4: Display the game --------------}
+
+display : (Int,Int) -> Game -> Element
+display (w,h) g = asText g
 
 
 delta = fps 30
-input = sampleOn delta (lift2 Input delta userInput)
+input = sampleOn delta (lift Input delta)
 
 gameState = foldp stepGame defaultGame input
 
--- mouse_chaser.elm
+++ mouse_chaser.elm
@@ -1,6 +1,9 @@
 import Window
 import Mouse
+import Random
 
+(width, height) = (400, 400)
+(hWidth, hHeight) = (width/2, height/2)
 
 delta = fps 30
 
@@ -9,21 +12,22 @@ relPos (x,y) (w,h) = (x - div w 2, -y + div h 2)
 
 {-- Part 1: Model the user input --------}
 
-type Input = { mPos:(Int, Int), time:Float }
+type Input = { mPos:(Int, Int), time:Float, rand:Float }
 
 
 input = sampleOn delta <| Input <~ lift2 relPos Mouse.position Window.dimensions
                                  ~ delta
+                                 ~ (Random.float delta)
 
 
 
 {-- Part 2: Model the game ---------------}
 
-type Game = {ball:Ball, cat:Cat}
+type Game = {ball:Ball, cat:Cat, kittens:[Cat]}
 type Ball = {pos:(Float, Float)}
 type Cat = {pos:(Float, Float), vel:(Float, Float)}
 
-defaultGame = {ball = defaultBall, cat = defaultCat}
+defaultGame = {ball = defaultBall, cat = defaultCat, kittens = []}
 defaultBall = {pos = (0,0)}
 defaultCat = {pos = (0,0), vel = (0,0)}
 
@@ -33,19 +37,34 @@ defaultCat = {pos = (0,0), vel = (0,0)}
 {-- Part 3: Update the game ---------------}
 
 stepGame : Input -> Game -> Game
-stepGame {mPos, time} g = {g | ball <- stepBall mPos g.ball,
-                               cat  <- stepCat  mPos g.cat}
+stepGame {mPos, time, rand} g = {g | ball <- stepBall mPos g.ball,
+                                     cat  <- stepCat  mPos g.cat,
+                                  kittens <- spawnKitten g.cat g.kittens rand}
 
 stepBall : (Int, Int) -> Ball -> Ball
 stepBall mPos b = { b | pos <- (fst mPos |> toFloat,
                                 snd mPos |> toFloat) }
 
 stepCat : (Int, Int) -> Cat -> Cat
-stepCat mPos c = { c | pos <- tupAdd c.pos c.vel,
+stepCat mPos c = { c | pos <- tupAdd c.pos c.vel 1,
                        vel <- tupVel c.pos (fst mPos |> toFloat,
                                             snd mPos |> toFloat)}
 
-tupAdd (ax,ay) (bx,by) = (ax + bx, ay + by)
+spawnKitten : Cat -> [Cat] -> Float -> [Cat]
+spawnKitten c kittens r = let kitten = {defaultCat | pos <- c.pos,
+                                                     vel <- c.vel}
+                              updatedKittens = stepKittens kittens
+                          in if length kittens < 50 && r < 0.05
+                             then kitten::updatedKittens
+                             else updatedKittens
+
+stepKittens : [Cat] -> [Cat]
+stepKittens kittens = map (\k -> {k | pos <- tupAdd k.pos k.vel 3})
+                      <| filter (\k -> onScreen k.pos) kittens
+
+onScreen (x,y) = x < hWidth && x > -hWidth && y < hHeight && y > -hHeight
+
+tupAdd (ax,ay) (bx,by) s = (ax + s * bx, ay + s * by)
 
 
 tupVel : (Float, Float) -> (Float, Float) -> (Float, Float)
@@ -66,9 +85,12 @@ display (w,h) g = let bPos = g.ball.pos
                       drawCat = square 40  |> filled black
                                            |> move cPos
                                            |> rotate ((fst cPos)/10)
+                      drawKitten k = square 10  |> filled blue
+                                                |> move k.pos
+                                                |> rotate ((fst k.pos)/5)
                                                     
                   in container w h middle <| color gray
-                                          <| collage 400 400 [drawBall, drawCat]
+                                          <| collage width height (drawBall::drawCat::(map drawKitten g.kittens))
 
 
 gameState = foldp stepGame defaultGame input
